81be51b8f177861c149637936bddb14e04ed251d
Abhishek Shankar Choudhary <theabhishek.srm@gmail.com>
2024-04-17 22:32:04 +0530

    selection

---
+++ app.js
@@ -0,0 +1,36 @@
+
+//  selection , how to select a html elment in js
+
+// document.querySelector("h1")
+
+// querselctor: 
+// // 1. single element
+// let heading1 = document.querySelector("h1") 
+
+// // 2. multiple element
+// let heading2 = document.querySelectorAll("h1")
+
+
+// // 3. by id
+// let heading3 = document.querySelector("#special")
+
+// // 4. by class
+// let heading4 = document.querySelectorAll(".dance")
+
+// // selected div: 
+// let div_js = document.querySelector("div")
+
+// // selected h1 with div_js
+// let heading5 = div_js.querySelector("h1")
+
+// innerText and innerHTML
+
+
+// let div_js = document.querySelector("div")
+// console.log(div_js)
+// console.log(div_js.innerText)
+// console.log(div_js.innerHTML)
+
+// let heading1 = document.querySelector("h1") 
+// // heading1.innerText = "I am changed"
+// heading1.innerHTML = "I am <span>changed</span> again"
